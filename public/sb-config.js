d1f1e82af6912051ff1347e0a4445ba7bcffa731
Engecapservicos <jhony@engecapservicos.com.br>
2026-08-27 21:14:24 -0300

    Update sb-config.js

--- public/sb-config.js
+++ public/sb-config.js
@@ -1,3 +1,3 @@
 // Cole aqui as chaves ANON do Supabase (Settings → API → legacy anon)
 window.SB_URL = "https://iktmegemzmmdnosembdw.supabase.co";
-window.SB_ANON = "COLE_A_CHAVE_ANON_AQUI";
+window.SB_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrdG1lZ2Vtem1tZG5vc2VtYmR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3OTYwNTQsImV4cCI6MjEwMzM3MjA1NH0.juulrOH6gUItYpKokhmeeO5db-A8kDgB0kdnFQO_Pco";
